1c24effd1ee0d85ac5d2d16ef9aa7c5c6abfbd6b
Meghan V. O'Neill <u0759157@utah.edu>
2022-10-27 03:10:35 -0600

    Added search url and params variables. Identified error with OAuth2. Need to choose a way of implementing authorization; suggestions from documentation for Python are rauth and sanction.

--- data_collecting.py
+++ data_collecting.py
@@ -10,9 +10,13 @@ genius = lg.Genius(' ', skip_non_songs=True,
                    excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
 url = 'https://api.genius.com'
 
+search_url = url + "/search"
+
 headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN}
 
-result = requests.get(url, headers=headers)
+params = {}
+
+result = requests.get(url, params=params, headers=headers)
 
 
 # Borrowed from Ekene A. published in Towards Data Science:
@@ -21,11 +25,28 @@ def gather_lyric_data_from_genius_api(arr, k):
     c = 0
     for name in arr:
         try:
-            songs = (genius.search_artist(name, max_songs=k, sort='popularity')).songs
-            s = [song.lyrics for song in songs]
-            file.write("\n \n   <|endoftext|>   \n \n".join(s))
-            c += 1
-            print(f"Songs grabbed:{len(s)}")
+            artist = genius.search_artist(name, max_songs=k, sort='popularity')
+            print('hey')
+            print(artist)
+            # songs = artist.songs
+            # s = [song.lyrics for song in songs]
+            # file.write("\n \n   <|endoftext|>   \n \n".join(s))
+            # c += 1
+            # print(f"Songs grabbed:{len(s)}")
         except:
             print("Oops!", sys.exc_info()[1], "occurred.")
             print(f"some exception at {name}: {c}")
+
+
+def main():
+
+    artists = ['Armani White', 'Roddy Ricch']
+    k = 3
+
+    gather_lyric_data_from_genius_api(artists, k)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
